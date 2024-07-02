3cf32fa1c0ba0b2f97963cbde7d7e3ba7dbf725c
lnune80132 <165737031+lnune80132@users.noreply.github.com>
2024-07-01 21:48:41 -0600

    PARTE DE LUIS

--- Web_TrabajoFidelitas/Web_TrabajoFidelitas/Controllers/AutomovilController.cs
+++ Web_TrabajoFidelitas/Web_TrabajoFidelitas/Controllers/AutomovilController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using Web_TrabajoFidelitas.Entidades;
 using Web_TrabajoFidelitas.Models;
 
 namespace Web_TrabajoFidelitas.Controllers
@@ -10,17 +12,99 @@ namespace Web_TrabajoFidelitas.Controllers
     [FiltroSeguridad]
     public class AutomovilController : Controller
     {
+        AutomovilModel model = new AutomovilModel();
+
+        [HttpGet]
         public ActionResult MostrarAutomoviles()
         {
-            return View();
+            var respuesta = model.ConsultarAutomoviles();
+            if (respuesta.Codigo == 0)
+            {
+                return View(respuesta.Datos);
+            } else 
+            {
+                ViewBag.MsjPantalla = respuesta.Detalle;
+                return View(new List<Automovil>());
+            }
         }
+
+        // ---------------------- REGISTRO ----------------------
+
+        [HttpGet]
         public ActionResult NuevoAutomovil()
         {
+            CargarViewBagIdClientes();
+
             return View();
         }
-        public ActionResult ActualizarAutomovil()
+
+        [HttpPost]
+        public ActionResult NuevoAutomovil(Automovil entidad)
         {
-            return View();
+            var respuesta = model.AgregarAutomovil(entidad);
+            if (respuesta.Codigo == 0)
+            {
+                return RedirectToAction("MostrarAutomoviles");
+            }
+            else
+            {
+                ViewBag.MsjPantalla = respuesta.Detalle;
+                return View(new List<Automovil>());
+            }
+        }
+
+        // ---------------------- ACTUALIZAR ---------------------
+
+        [HttpGet]
+        public ActionResult ActualizarAutomovil(long id)
+        {
+            CargarViewBagIdClientes();
+            var respuesta = model.ConsultarAutomovil(id);
+            return View(respuesta.Dato);
+        }
+        [HttpPost]
+        public ActionResult ActualizarAutomovil(Automovil entidad)
+        {
+            var respuesta = model.ActualizarAutomovil(entidad);
+            if (respuesta.Codigo == 0)
+            {
+                return RedirectToAction("MostrarAutomoviles");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // ---------------------- ELIMINAR ---------------------
+
+        [HttpGet]
+        public ActionResult EliminarAutomovil(long id)
+        {
+            var respuesta = model.EliminarAutomovil(id);
+
+            if (respuesta.Codigo == 0)
+            {
+                return RedirectToAction("MostrarAutomoviles", "Automovil");
+            }
+            else
+            {
+                ViewBag.MsjPantalla = respuesta.Detalle;
+                return View();
+            }
+        }
+
+        // ---------------------- EXTRA / DROPDOWN ----------------------
+        private void CargarViewBagIdClientes()
+        {
+            var respuesta = model.ConsultarIdsClientes();
+            var idClientes = new List<SelectListItem>();
+
+            idClientes.Add(new SelectListItem { Text = "seleccione un id ...", Value = "" });
+            foreach (var item in respuesta.Datos)
+                idClientes.Add(new SelectListItem { Text = item.NombreCliente, Value = item.IdsCliente.ToString() });
+
+            ViewBag.IdsClientes = idClientes;
         }
     }
 }
